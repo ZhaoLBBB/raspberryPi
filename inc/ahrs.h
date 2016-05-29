476b5dc2b965a12b9f1aee99bda2d2ecdb3926f9
ZhaoLBBB <zhaolei_buaa@163.com>
2016-05-29 09:31:09 +0000

    add AHRS algorithm

--- inc/ahrs.h
+++ inc/ahrs.h
@@ -5,13 +5,17 @@
 class ahrs{
 	public:
 		ahrs(float sampletime);
-		void  update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
-		void  update(float gx, float gy, float gz, float ax, float ay ,float az);
+		void  MahonyAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
+		void  MahonyAHRSupdate(float gx, float gy, float gz, float ax, float ay ,float az);
 		float invSqrt(float x);
 		void toEuler(float *thetax, float *thetay, float *thetaz);
+		
+		void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az); 
+		void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz); 
 		float q0, q1, q2, q3;
 		float twoKp, twoKi;
 		float integralFBX, integralFBY, integralFBZ;
 		float sampleTime;
+		float beta;
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include "AKM8975.h"
 #include "I2C.h"
 #include "ahrs.h"
+#include <time.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 MPU6050 accelgyro;
 AKM8975 mag;
-ahrs MahonyAHRS(0.05f);
+ahrs AHRS(0.001f);
+struct timespec ts;
 float ax, ay, az, gx, gy, gz;
 //int16_t mx, my, mz;
 float yaw, pitch, roll;
 float axf, ayf, azf, gxf, gyf, gzf;
 int main(int argc, char **argv)
 {
+	ts.tv_sec = 0;
+	ts.tv_nsec = 1* (1e-3)/(1e-9);
 	if(accelgyro.testConnection()){
 		printf("Connection OK\n");
 	}
@@ -21,12 +25,13 @@ int main(int argc, char **argv)
 //	if(mag.testConnection())
 //		printf("AKM8975 connect OK\n");
 	while(1){
-		usleep(50000);
+		nanosleep(&ts, NULL);
 		accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 //		mag.getMagnetic(&mx, &my, &mz);
-		printf("ax:%f,	ay:%f,	az:%f,	gx:%f,	gy:%f, gz:%f\n", ax, ay, az, gx, gy, gz);
-		MahonyAHRS.update(gx, gy, gz, ax, ay, az);
-		MahonyAHRS.toEuler(&yaw, &pitch, &roll);
+//		printf("ax:%f,	ay:%f,	az:%f,	gx:%f,	gy:%f, gz:%f\n", ax, ay, az, gx, gy, gz);
+		//AHRS.MahonyAHRSupdate(gx, gy, gz, ax, ay, az);
+		AHRS.MadgwickAHRSupdate(gx, gy, gz, ax, ay, az);
+		AHRS.toEuler(&yaw, &pitch, &roll);
 		printf("YAW:%f,\tPITCH:%f,\tROLL:%f\n", yaw*57.32f, pitch * 57.32f, roll * 57.32f);
 	}
 	return 0;
